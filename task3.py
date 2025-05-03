291301d74551ad07a87524d8929319c262868698
codea-n <ali.hamza12714@gmail.com>
2025-05-03 01:23:28 -0500

    "task3_started"

---
+++ task3.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import pandas as pd
+import dash
+from dash import dcc, html
+import plotly.express as px
+
+
+#task 2
+daily_sales_data_0 = pd.read_csv('data/daily_sales_data_0.csv')
+daily_sales_data_1 = pd.read_csv('data/daily_sales_data_1.csv')
+daily_sales_data_2 = pd.read_csv('data/daily_sales_data_2.csv')
+
+merged_df=pd.concat([daily_sales_data_0, daily_sales_data_1, daily_sales_data_2], ignore_index=True)
+merged_df=merged_df[merged_df['product']=='pink morsel']
+
+new_merged_df=merged_df.copy()
+new_merged_df['price']=new_merged_df['price'].astype(str)
+new_merged_df['price']=new_merged_df['price'].str.replace('$','')
+new_merged_df['price']=new_merged_df['price'].astype(float)
+new_merged_df['Sales']=new_merged_df['price']*new_merged_df['quantity']
+daily_sales_data=new_merged_df.copy()
+daily_sales_data.drop(columns=['product','price','quantity'], inplace=True)
+print(daily_sales_data.head(5))
+
+
+#task3
+app = dash.Dash(__name__)
+

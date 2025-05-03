788b9724416ef13e65a764c1af61592e8a2b0706
codea-n <ali.hamza12714@gmail.com>
2025-05-03 14:35:27 -0500

    task3

--- task3.py
+++ task3.py
@@ -25,4 +25,12 @@ print(daily_sales_data.head(5))
 
 #task3
 app = dash.Dash(__name__)
-
+app.layout = html.Div([
+    html.H1("Sales Data Visualization"),
+    dcc.Graph(
+        id='sales-graph',
+        figure=px.line(daily_sales_data, x='date', y='Sales', title='Sales Over Time')
+    )
+])
+if __name__ == '__main__':
+    app.run(debug=True)
